88f2a31e424d4a4dc71de5cfaa4fe9702704fcc3
contre_a <contre_a@epitech.eu>
2014-12-18 12:37:51 +0100

    implement insert

--- index.php
+++ index.php
@@ -11,6 +11,14 @@ include ('includes/bdd.php');
 
 $filterUser = -1;
 
+if (isset($_POST['addTask'])) {
+	$sql = "INSERT INTO task (task, user_id, status) VALUES ('" . addslashes($_POST['task']) . "', '" . $_POST['user_id'] . "', 0)";
+	$stmt = $bdd->prepare($sql);
+	$stmt->execute();
+
+	$filterUser = $_POST['user_id'];
+}
+
 $sqlWhere = "";
 if (isset($_POST['user']) && $_POST['user'] != -1 || $filterUser != -1) {
 	if ($filterUser == -1) {
