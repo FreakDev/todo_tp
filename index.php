d3438918b67a8bcb6b0a97ad04f1146a9ef02265
contre_a <contre_a@epitech.eu>
2014-12-18 12:06:18 +0100

    implement basic task list

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
+include ('includes/bdd.php');
 
+$sql = "SELECT * FROM task ORDER BY status ASC";
+$stmt = $bdd->prepare($sql);
+$stmt->execute();
+$dataTasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$sql = "SELECT * FROM user";
+$stmt = $bdd->prepare($sql);
+$stmt->execute();
+$dataUsers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 include ('templates/index.phtml');
 
